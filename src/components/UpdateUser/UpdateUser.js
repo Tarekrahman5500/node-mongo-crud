9012f160b039e9fe611a121b833da09606919703
tr617 <tr617739@gmail.com>
2022-06-30 23:15:46 +0600

    UI for mongodb curd update operation

--- src/components/UpdateUser/UpdateUser.js
+++ src/components/UpdateUser/UpdateUser.js
@@ -5,7 +5,7 @@ import axios from "axios";
 const UpdateUser = () => {
 
     const {id} = useParams()
-    const [user, setUser] = useState([])
+    const [user, setUser] = useState({})
     const LoadData = () => {
         (async () => {
             try {
@@ -17,10 +17,49 @@ const UpdateUser = () => {
         })()
     }
     useEffect(LoadData, [id])
+
+    const handleUpdate = e => {
+        e.preventDefault()
+        const url = `http://localhost:5000/users/${id}`
+        axios.put(url, user)
+            .then(response => {
+
+                if(response.data.modifiedCount > 0) {
+                        window.alert('update user successfully')
+                        setUser({})
+                }
+            })
+
+    }
+
+    const handleNameChange = e => {
+        const updateName = e.target.value
+        const updateUser = {name:updateName, email: user.email}
+        setUser(updateUser)
+    }
+
+    const handleEmailChange = e => {
+
+        const updateEmail = e.target.value
+        const updateUser = {...user}
+        updateUser.email = updateEmail
+        setUser(updateUser)
+
+    }
     return (
         <div>
             <h2> Update: {user.name}</h2>
             <p><small>{id}</small></p>
+
+            <form onSubmit={handleUpdate}>
+                <input type="text" onChange={handleNameChange} value={user.name || ' '}/>
+                <br/>
+                <br/>
+                <input type="email" onChange={handleEmailChange} value={user.email || ' '}/>
+                <br/>
+                <input type="submit" value="Update"/>
+
+            </form>
         </div>
     );
 };
